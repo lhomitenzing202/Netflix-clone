63142f94c40cb282a3386d392c4fe80d6f4f196c
lhomitenzing202 <tenzinglhomi15@gmail.com>
2024-01-20 22:14:40 +0545

    nav completed

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
-export default {
-  plugins: {
+// postcss.config.js
+export const plugins = {
     tailwindcss: {},
     autoprefixer: {},
-  },
-}
+};
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,28 @@
+
 import { Link } from "react-router-dom"
 
 
 const Navbar = () => {
   return (
 
-   <div>
+   <div >
+
     <Link to ="/">
-    <h1 className= "uppercase text-red-700 font-nsans-bold cursor-pointer text-5xl">
+    <h1 className= "text-transform: uppercase; text-red-700 font-nsans-bold cursor-pointer text-5xl">
         Netflix</h1>
     </Link>
+
+    <div className="absolute w-full flex items-center justify-between z-50">
+      <Link to="/login">
+       <button className="capitalize pr-4">Login</button>
+      </Link>
+
+      <Link to="/signup">
+       <button className="capitalize bg-red-700 px-6 py-2 rounded cursor-pointer">Signup</button>
+      </Link>
+    </div>
    </div>
+
   );
 };
 
