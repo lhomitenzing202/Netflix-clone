c5572bd75339adc571884aca23818fe566722fd8
lhomitenzing202 <tenzinglhomi15@gmail.com>
2024-01-22 22:09:50 +0545

    commit in hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import endpoints from "../Services/Movieservices";
 
 
 const Hero = () => {
+  const[movie, setMovie]=useState({});
+
+  useEffect(()=>{
+    axios.get(endpoints.popular).then((response)=>{
+      const movies = response.data.results;
+      const randomMovie = movies[Math.floor(Math.random()*movies.length)];
+      setMovie(randomMovie);
+    });
+
+  },[])
+  if(!movie)
+  return(
+<>
+<p>fetching movie...</p>
+</>
+);
+
+const{title,back_droppath,release_date, overview} = movie;
+
   return (
-    <div>Hero</div>
+    <div className="w-full h-[550px] lg:h[850px] ">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] lg:h[850px] bg-gradient-to-r from-black">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
