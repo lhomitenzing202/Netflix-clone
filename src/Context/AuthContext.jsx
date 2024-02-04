10edbefbb7e1444eb80bb30110fc459199ee5e9f
lhomitenzing202 <tenzinglhomi15@gmail.com>
2024-02-05 00:10:43 +0545

    2nd commit

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,7 +1,22 @@
-import { useContext } from 'react'
+/* eslint-disable no-unused-vars */
+import {createContext, useContext, useState } from 'react'
+import{createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut, onAuthStateChanged} from 'firebase/auth'
+import{auth, db} from '../Services/Firebase'
 
-export const AuthContext = createContext();
 
-export default AuthContextProvider({Children})export function UserAuth(){
+const AuthContext = createContext();
+
+export function AuthContextProvider({Children}){
+    const[user, setUser]= useState({})
+
+    function signUp(email, password){
+        createuser
+    }
+
+
+    return
+       <AuthContext.Provider value={{}}>{Children}</AuthContext.Provider>;
+}
+export function UserAuth(){
     return useContext(AuthContext);
 }
